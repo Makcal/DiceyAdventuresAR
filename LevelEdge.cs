cf2afd68e98cd7b92d3a1479e1ab1ea0cd442dbe
QuuantumMax <62051211+QuuantumMax@users.noreply.github.com>
2020-10-23 18:02:11 +0300

    Add files via upload

---
+++ LevelEdge.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DiceyDungeonsAR.MyLevelGraph
+{
+    public class LevelEdge : MonoBehaviour
+    {
+        public Field startField;
+        public Field connectedField;
+        public int edgeWeight;
+ 
+        public void Initialize(Field startField, Field connectedField, int weight)
+        {
+            this.startField = startField;
+            this.connectedField = connectedField;
+            edgeWeight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"Edge from {startField} to {connectedField}";
+        }
+    }
+ 
+}

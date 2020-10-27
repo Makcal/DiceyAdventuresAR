b7df370a40032a669db29cf50055573bc5eebf15
Makcal <fominmaxim3721@gmail.com>
2020-10-27 19:47:30 +0300

    Edges system fix

--- Assets/Scripts/MyLevelGraph/LevelEdge.cs
+++ Assets/Scripts/MyLevelGraph/LevelEdge.cs
@@ -34,14 +34,14 @@ namespace DiceyDungeonsAR.MyLevelGraph
             edgeWeight = weight;
 
             var scale = transform.localScale;
-            scale.x *= (connectedField.transform.position - startField.transform.position).magnitude;
+            scale.z *= (connectedField.transform.position - startField.transform.position).magnitude;
             transform.localScale = scale;
 
             transform.rotation = Quaternion.LookRotation(connectedField.transform.position - startField.transform.position);
             var radians = transform.rotation.eulerAngles.y * Mathf.PI / 180;
 
-            var offsetX = new Vector3(scale.x / 2 * Mathf.Sin(radians), 0, 0);
-            var offsetZ = new Vector3(0, 0, scale.x / 2 * Mathf.Cos(radians));
+            var offsetX = new Vector3(scale.z / 2 * Mathf.Sin(radians), 0, 0);
+            var offsetZ = new Vector3(0, 0, scale.z / 2 * Mathf.Cos(radians));
             transform.position = startField.transform.position + offsetX + offsetZ;
 
             transform.parent = level.transform;

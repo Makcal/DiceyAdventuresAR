6463e1e8f16e446c237ab44e8c432697b4cb9c8b
Makcal <fominmaxim3721@gmail.com>
2020-12-01 17:20:21 +0300

    Added menu

---
+++ Assets/Scripts/Menu/EscapeGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Menu/MenuHandler1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler1 : MonoBehaviour
+{
+    public String gameSceneName;
+    public void StartApplication()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }  
+    
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("0");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("2");
+    }
+
+    public void Objects()
+    {
+        SceneManager.LoadScene("3");
+    }
+
+    public void Gameplay()
+    {
+        SceneManager.LoadScene("4");
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

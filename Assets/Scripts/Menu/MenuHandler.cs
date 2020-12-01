b49ae243b7f1d507f4f1195c15c84ba9c82d4e3b
Makcal <fominmaxim3721@gmail.com>
2020-12-01 17:51:48 +0300

    Scene fixes

---
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public String gameScene = "Main",
+        mainMenuScene = "MainMenu",
+        helpScene = "Help",
+        objectsScene = "Objects",
+        gameplayScene = "Gameplay";
+    public void StartApplication()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+    
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+    public void Help()
+    {
+        SceneManager.LoadScene(helpScene);
+    }
+
+    public void Objects()
+    {
+        SceneManager.LoadScene(objectsScene);
+    }
+
+    public void Gameplay()
+    {
+        SceneManager.LoadScene(gameplayScene);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

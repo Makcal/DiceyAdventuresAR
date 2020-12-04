627af95ae1ef57f6d11b439a83be82f5c5ed09fd
Makcal <fominmaxim3721@gmail.com>
2020-12-04 17:28:30 +0300

    Appearing anim

---
+++ Assets/Scripts/2D/AppearingAnim.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AppearingAnim : MonoBehaviour
+{
+    bool started = false;
+    float startTime;
+    float startHeight;
+    Graphic g;
+    public Color color;
+    public float period = 1;
+    public float yOffset = 0;
+
+    private void Start()
+    {
+        g = GetComponent<Graphic>();
+
+        Play();
+    }
+
+    public void Play()
+    {
+        started = true;
+        startTime = Time.time;
+        startHeight = transform.localPosition.y;
+    }
+
+    void Update()
+    {
+        if (!started)
+            return;
+
+        float a = Mathf.Min((Time.time - startTime) / period, 2);
+
+        g.color = new Color(color.r, color.g, color.b, a>1 ? 2-a : a);
+
+        var pos = transform.localPosition;
+        pos.y = startHeight + yOffset * a;
+        transform.localPosition = pos;
+    }
+}

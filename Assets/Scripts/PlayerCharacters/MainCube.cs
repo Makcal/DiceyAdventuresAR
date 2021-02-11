3ab75607c0164dbe0b507430e16937949a999b25
Makcal <fominmaxim3721@gmail.com>
2021-02-11 15:39:04 +0300

    Update to 2020.2.3f1 Unity version and error fix

--- Assets/Scripts/PlayerCharacters/MainCube.cs
+++ Assets/Scripts/PlayerCharacters/MainCube.cs
@@ -6,6 +6,6 @@ namespace DiceyDungeonsAR.GameObjects.Players
     public class MainCube : Player
     {
         public override int StartHealth { get; } = 24;
-        public override int UpgradeHealth { get; protected set; } = 4;
+        public override int UpgradeHealth { get; } = 4;
     }
 }
